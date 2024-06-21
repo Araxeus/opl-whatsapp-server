1cd334319a6348b726a0e5d6d90171d064149945
Araxeus <78568641+Araxeus@users.noreply.github.com>
2024-06-21 23:51:48 +0300

    Update build.ts

--- build.ts
+++ build.ts
@@ -4,7 +4,7 @@ if (test) {
     console.log('Building test.js...');
     await Bun.build({
         entrypoints: ['./src/test.ts'],
-        outdir: 'dist',
+        outdir: './dist',
         target: 'node',
         //minify: true,
         sourcemap: 'inline', //external',
@@ -13,24 +13,26 @@ if (test) {
     console.log('test.js build complete');
 } else {
     console.log('building server.js');
-    await Bun.build({
+    const serverRes = await Bun.build({
         entrypoints: ['./src/server.ts'],
-        outdir: 'dist',
+        outdir: './dist',
         target: 'node',
         minify: true,
         sourcemap: 'external', // external
         external: ['@whiskeysockets/baileys'],
     });
     console.log('server.js build complete');
+    console.log(serverRes);
     console.log('building fetch-and-qr.js');
-    await Bun.build({
+    const frontRes = await Bun.build({
         entrypoints: ['./pages/fetch-and-qr.ts'],
-        outdir: 'dist',
+        outdir: './dist',
         target: 'browser',
         minify: true,
         sourcemap: 'external', // external
     });
     console.log('fetch-and-qr.js build complete');
+    console.log(frontRes);
 }
 
 //console.log(`Build result:\n${JSON.stringify(result, null, 2)}`);
