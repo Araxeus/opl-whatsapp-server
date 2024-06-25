191db07f02364777635e675392da2e6248a82516
Araxeus <78568641+Araxeus@users.noreply.github.com>
2024-06-25 17:17:44 +0300

    timeout 1sec before routine

--- src/whatsapp/index.ts
+++ src/whatsapp/index.ts
@@ -87,22 +87,25 @@ export async function handleWhatsappRoutine(
             resolve({ qrCode });
         });
         instance.once('open', () => {
-            log.info('Instance opened');
             setLastAuth(user.userID);
-            instance
-                .routine(data)
-                .then((res) => {
-                    resolve(res);
-                })
-                .catch((e) => {
-                    log.error(`Error: ${JSON.stringify(e, null, 2)}`);
-                    resolve({ success: false, error: e });
-                })
-                .finally(() => {
-                    setTimeout(() => {
-                        instance.close();
-                    }, 1500);
-                });
+            setTimeout(
+                () =>
+                    instance
+                        .routine(data)
+                        .then((res) => {
+                            resolve(res);
+                        })
+                        .catch((e) => {
+                            log.error(`Error: ${JSON.stringify(e, null, 2)}`);
+                            resolve({ success: false, error: e });
+                        })
+                        .finally(() => {
+                            setTimeout(() => {
+                                instance.close();
+                            }, 1500);
+                        }),
+                1000,
+            );
         });
     });
 }
