271a5ae88cbbb9143413ce5f8ff899874d9af4d7
Araxeus <78568641+Araxeus@users.noreply.github.com>
2024-08-02 16:59:08 +0300

    check that patchFileContent() path is within project directory

--- patches/index.ts
+++ patches/index.ts
@@ -5,7 +5,14 @@ export async function patchFileContent(
     filepath: string,
     replacer: (scriptContent: string) => string,
 ) {
+    if (!filepath || filepath.includes('\0')) {
+        throw new Error('Invalid filepath');
+    }
     const realpath = path.resolve(import.meta.dir, filepath);
+    // check that realpath is within the project directory
+    if (!realpath.startsWith(path.resolve(import.meta.dir, '..'))) {
+        throw new Error(`Invalid filepath: ${realpath}`);
+    }
     const scriptContent = await readFile(realpath, 'utf-8');
     const patchedContent = replacer(scriptContent);
     await writeFile(realpath, patchedContent);
