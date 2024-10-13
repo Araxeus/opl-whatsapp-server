425b145b1ba6393efe09af7ec69e65a42532d54f
Araxeus <oaeben@gmail.com>
2024-10-13 08:41:26 +0300

    need squash

--- src/whatsapp/park-car.ts
+++ src/whatsapp/park-car.ts
@@ -4,7 +4,7 @@ import {
     type Question,
     buttonSelector,
     listMessageDescriptionSelector,
-    listMessageTitleSelector,
+    //listMessageTitleSelector,
 } from 'whatsapp/shared';
 import { z } from 'zod';
 
